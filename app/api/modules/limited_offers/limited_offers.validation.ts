963dffa56e803a15f8ba0b7c170ca83cb795281f
Altin Musahu <altin@fron.agency>
2026-06-17 15:23:55 +0200

    fixed

--- app/api/modules/limited_offers/limited_offers.validation.ts
+++ app/api/modules/limited_offers/limited_offers.validation.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const createLimitedOffers = z.object({
   plan_mobile_id: z.string().min(1, "Provider is required"),
-  email: z.string().min(1, "Email is required"),
+  email: z.string().email("Invalid email"),
 })
 
 export type CreateLimitedOffers = z.infer<typeof createLimitedOffers>
